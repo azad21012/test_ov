d435afc6452bdadc16f7e689ebea95c64e4f6f20
zarshad <a.zarshad@rayatak.ir>
2021-01-05 13:39:00 +0330

    add

---
+++ precache-manifest.ab9363478e637345475bdeb400f6d360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ecbe5047e6574acb670c3464339fce",
+    "url": "/kurento/index.html"
+  },
+  {
+    "revision": "dfdd166da406d4c932ea",
+    "url": "/kurento/static/css/main.5abfa4cd.chunk.css"
+  },
+  {
+    "revision": "e70f1fe606324b0b62ce",
+    "url": "/kurento/static/js/2.4fed143c.chunk.js"
+  },
+  {
+    "revision": "fe2438b82eb149af25fd1fde5cd744d9",
+    "url": "/kurento/static/js/2.4fed143c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdd166da406d4c932ea",
+    "url": "/kurento/static/js/main.6c846d3d.chunk.js"
+  },
+  {
+    "revision": "3017de6ba381f52330f4",
+    "url": "/kurento/static/js/runtime-main.7e5e075c.js"
+  }
+]);
