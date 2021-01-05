5815ef0a553e29e0543003144944151375a232df
zarshad <a.zarshad@rayatak.ir>
2021-01-05 13:28:07 +0330

    add

---
+++ precache-manifest.9425b369343844ea9395adf06b953ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bbb12f7a48a2da251d0f762a3c7bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67656b1080ab7bff0740",
+    "url": "/static/css/main.5abfa4cd.chunk.css"
+  },
+  {
+    "revision": "e70f1fe606324b0b62ce",
+    "url": "/static/js/2.4fed143c.chunk.js"
+  },
+  {
+    "revision": "fe2438b82eb149af25fd1fde5cd744d9",
+    "url": "/static/js/2.4fed143c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67656b1080ab7bff0740",
+    "url": "/static/js/main.bee18f05.chunk.js"
+  },
+  {
+    "revision": "172a8a939c19d2477e4d",
+    "url": "/static/js/runtime-main.8b8b8440.js"
+  }
+]);
